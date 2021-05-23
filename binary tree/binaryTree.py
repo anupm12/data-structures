dca3ccaf580a5e89a3c0c27ec4b0847945d14239
anupm12 <shuklaanupam18@gmail.com>
2021-05-23 14:36:34 +0530

    Binary tree

---
+++ binary tree/binaryTree.py
@@ -0,0 +1,87 @@
+class Node:
+    def __init__(self, value = None):
+        self.value = value
+        self.leftChild = None
+        self.rightChild = None
+
+class binaryTree:
+    root = None
+
+    def insert(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+            while(True):
+                if val < current.value:
+                    if current.leftChild == None:
+                        current.leftChild = Node(val)
+                        return
+                    current = current.leftChild
+                else:
+                    if current.rightChild == None:
+                        current.rightChild = Node(val)
+                        return
+                    current = current.rightChild
+
+    def find(self, key):
+        current = self.root
+        while(current != None):
+            if key < current.value:
+                current = current.leftChild
+            elif key > current.value:
+                current = current.rightChild
+            else:
+                return 1
+        return 0
+
+    def traversePreOrder(self):
+        self.__traversePreOrder(self.root)
+
+    def __traversePreOrder(self, root):
+        if root == None:
+            return
+        print(root.value)
+        self.__traversePreOrder(root.leftChild)
+        self.__traversePreOrder(root.rightChild)
+    
+    def traverseInOrder(self):
+        self.__traverseInOrder(self.root)
+
+    def __traverseInOrder(self, root):
+        if root == None:
+            return
+        self.__traverseInOrder(root.leftChild)
+        print(root.value)
+        self.__traverseInOrder(root.rightChild)
+    
+    def traversePostOrder(self):
+        self.__traversePostOrder(self.root)
+
+    def __traversePostOrder(self, root):
+        if root == None:
+            return
+        self.__traversePostOrder(root.leftChild)
+        self.__traversePostOrder(root.rightChild)
+        print(root.value)
+
+bt = binaryTree()
+
+#INSERT
+bt.insert(10)
+bt.insert(5)
+bt.insert(20)
+bt.insert(15)
+bt.insert(25)
+
+#FIND
+# print(bt.find(20))
+
+#TRAVERSE - PRE ORDER
+# bt.traversePreOrder()
+
+#TRAVERSE - IN ORDER
+# bt.traverseInOrder()
+
+#TRAVERSE - POST ORDER
+# bt.traversePostOrder()
